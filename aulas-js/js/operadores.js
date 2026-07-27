ac9343e08cd2973cccda19925f810c437a5a7459
Willigham Coelho <willigamcoelho@gmail.com>
2026-07-27 16:04:34 -0300

    aula 4, 5, 6 - js

---
+++ aulas-js/js/operadores.js
@@ -0,0 +1,30 @@
+// Operadores em JavaScript
+var valor1, valor2, total;
+valor1 = 5;
+valor2 = 2;
+total = valor1 + valor2; // operador de adição
+total = valor1 - valor2; // operador de subtração
+total = valor1 * valor2; // operador de multiplicação
+total = valor1 / valor2; // operador de divisão
+total = valor1 % valor2; // operador de módulo (resto da divisão)
+total = valor1 ** valor2; // operador de exponenciação
+alert(total);
+
+valor += valor2; // operador de atribuição com adição
+valor -= valor2; // operador de atribuição com subtração
+valor *= valor2; // operador de atribuição com multiplicação
+valor /= valor2; // operador de atribuição com divisão
+valor %= valor2; // operador de atribuição com módulo
+valor **= valor2; // operador de atribuição com exponenciação
+
+comparacao = valor1 == valor2; // operador de igualdade
+comparacao = valor1 != valor2; // operador de desigualdade
+comparacao = valor1 === valor2; // operador de igualdade estrita (verifica valor e tipo)
+comparacao = valor1 !== valor2; // operador de desigualdade estrita (verifica valor e tipo)
+comparacao = valor1 > valor2; // operador de maior que
+comparacao = valor1 < valor2; // operador de menor que
+comparacao = valor1 >= valor2; // operador de maior ou igual 
+comparacao = valor1 <= valor2; // operador de menor ou igual 
+comparacao = valor1 && valor2; // operador lógico AND
+comparacao = valor1 || valor2; // operador lógico OR
+comparacao = !valor1; // operador lógico NOT
