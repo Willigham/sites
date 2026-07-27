730c5e72c0328db0a943c82a4f56eb710162eaef
Willigham Coelho <willigamcoelho@gmail.com>
2026-07-27 10:41:17 -0300

    Aula 2 - js

--- aulas-js/js/index.js
+++ aulas-js/js/index.js
@@ -1,1 +1,5 @@
-alert("Um alerta rodando em uma página externa");
+document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>!";
+
+alert( "Oi, isso é um alerta!" ); //alert parece na tela do usuário
+
+console.log("Oi, isso é um log!"); //console.log aparece no console do navegador
