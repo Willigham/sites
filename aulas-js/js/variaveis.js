0017c4633dafe70ecb9b24b06642d8f2a8194abd
Willigham Coelho <willigamcoelho@gmail.com>
2026-07-27 15:43:35 -0300

    aula 3 - js

--- aulas-js/js/variaveis.js
+++ aulas-js/js/variaveis.js
@@ -1,1 +1,24 @@
 let a, b, c; a = 5; b = 6; c = 7; //a declarção é saparada por vírgula, mas a atribuição é separada por ponto e vírgula
+
+//exemplo de declaração de variáveis com atribuição
+var nome = "Willigham"; //variável do tipo string
+
+var idade = 20;
+
+var altura = 1.80; //variável do tipo number
+
+var estudante = true; //variável do tipo boolean
+
+var sobrenome; //variável do tipo undefined
+
+var cidade = null; //variável do tipo null
+
+var simbolo = Symbol("simbolo"); //variável do tipo symbol
+
+var objeto = { nome: "Willigham", idade: 20, altura: 1.80 }; //variável do tipo object
+
+var array = [1, 2, 3, 4, 5]; //variável do tipo array
+
+var funcao = function() { return "Olá, mundo!"; }; //variável do tipo function
+
+
