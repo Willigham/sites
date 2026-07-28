8be3a2ea3a1b965ff38409a52f172c57ea001ed1
Willigham Coelho <willigamcoelho@gmail.com>
2026-07-28 14:45:54 -0300

    aula 14 - js

---
+++ aulas-js/js/switch.js
@@ -0,0 +1,16 @@
+// criando uma função switch
+function verificarDia(cor) {
+    switch (cor) {
+        case "vermelho":
+            console.log("A cor é vermelho");
+            break;
+        case "azul":
+            console.log("A cor é azul");
+            break;
+        case "verde":
+            console.log("A cor é verde");
+            break;
+        default:
+            console.log("A cor não é vermelho, azul ou verde");
+    }
+}
